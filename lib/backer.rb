70c9d8b8a6f8dc624c49e19923da481fbc37f6dc
Paige Wiener <snowplushie@yahoo.com>
2019-03-28 00:28:05 +0000

    Done.

---
+++ lib/backer.rb
@@ -0,0 +1,16 @@
+class Backer 
+  attr_reader :backed_projects
+  attr_accessor :name
+  
+  def initialize (name)
+    @name = name
+    @backed_projects = []
+  end
+  
+  def back_project(project)
+    @backed_projects << project
+    project.backers << self
+  end
+end
+    
+    
