4be2dbd44275fea4b4f9c180342256f66391d2ae
Paige Wiener <snowplushie@yahoo.com>
2019-03-28 00:12:59 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -1,7 +1,7 @@
 class Backer 
   attr_reader :backed_projects, :name
   
-  def initialize (backed_projects)
+  def initialize (name)
     @backed_projects = backed_projects
     @name = name
   end
